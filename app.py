398423056405f116c7d3773c1cd24231d8b62e96
Andrey Leonov <you@example.com>
2025-05-23 12:48:20 +0300

    app.py fix1

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from lab1.app.app import app as lab1_app
 from lab2.app.app import app as lab2_app
-from lab3.app.app import app as lab3_app
-from lab4.app import app as lab4_app
 
 from root_app.app import app as root_app
 
